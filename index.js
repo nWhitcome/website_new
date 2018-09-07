bf63b68fc4660a7b6243d3d70d44fe9c89eedfe1
Nathan Whitcome <nathanw315@gmail.com>
2018-09-06 23:05:08 -0500

    updated design

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var port = 3000;
+var server = require('http').Server(app);
+
+server.listen(port, "127.0.0.1", function () {
+});
+
+/*
+	Initializes express.
+*/
+app.use(express.static(__dirname));
+
+/*
+	Draws the index.html file as soon as the page loads.
+*/
+app.get('/', function(request, response){
+	response.sendFile(__dirname + '/index.html')
+});
