3c657768fbf94126582462c50d856a346f34db5b
nWhitcome <whitcome@iastate.edu>
2019-03-11 23:28:37 -0500

    added some more content and removed bottom bar for now

--- src/vueFile.js
+++ src/vueFile.js
@@ -2,49 +2,10 @@
 
 const store = new Vuex.Store({
 	state: {
-		picture_links: ['src/background_pictures/my_back_mountain.jpg', 
-			'src/background_pictures/Badlands.jpg', 
-			'src/background_pictures/Colorado_Top.jpg', 
-			'src/background_pictures/Glacier1.jpg', 
-			'src/background_pictures/Glacier2.jpg', 
-			'src/background_pictures/Badlands2.jpg', 
-			'src/background_pictures/Swing_Dance.jpg', 
-			'src/background_pictures/Tetons1.jpg', 
-			'src/background_pictures/Tetons2.jpg',
-			'src/background_pictures/Tetons3.jpg',
-			'src/background_pictures/River.jpg',
-			'src/background_pictures/Campsite.jpg'],
-			current_picture_index: 0,
-			current_timer: 0,
-			slide_direction: "slide_left"
+		bottom_visible: 0,
+		current_picture_index: 0,
 	},
-	mutations:{
-		setTimer(state){
-			state.current_timer = setInterval(store.commit('onClickForward'), 10000);
-		},
-		stopTimer(state){
-			clearInterval(state.current_timer);
-		},
-		onClickBack(state) {
-			state.slide_direction = "slide_right";
-			if (state.current_picture_index == 0) {
-				state.current_picture_index = state.picture_links.length - 1;
-			}
-			else
-				state.current_picture_index = state.current_picture_index - 1;
-			clearInterval(state.current_timer);
-			state.current_timer = setInterval(state.onClickForward, 10000);
-		},
-		onClickForward(state) {
-			console.log('ACTIVE')
-			state.slide_direction = "slide_left";
-			if (state.current_picture_index == state.picture_links.length - 1)
-				state.current_picture_index = 0;
-			else
-				state.current_picture_index = state.current_picture_index + 1;
-			clearInterval(state.current_timer);
-			state.current_timer = setInterval(state.onClickForward, 10000);
-		},
+	mutations: {
 	},
 })
 
@@ -55,26 +16,7 @@ Vue.component('top_bar', {
 		}
 	},
 	methods: {
-		onClickBack(event) {
-			this.slide_direction = "slide_right";
-			if (this.current_picture_index == 0) {
-				this.current_picture_index = this.picture_links.length - 1;
-			}
-			else
-				this.current_picture_index = this.current_picture_index - 1;
-			clearInterval(this.current_timer);
-			this.current_timer = setInterval(this.onClickForward, 10000);
-		},
-		onClickForward(event) {
-			this.slide_direction = "slide_left";
-			if (this.current_picture_index == this.picture_links.length - 1)
-				this.current_picture_index = 0;
-			else
-				this.current_picture_index = this.current_picture_index + 1;
-			clearInterval(this.current_timer);
-			this.current_timer = setInterval(this.onClickForward, 10000);
-		},
-		headingClicked(buttonsVisible, text){
+		headingClicked(buttonsVisible, text) {
 			this.$root.photo_buttons_visible = buttonsVisible;
 			this.selected = text;
 		}
@@ -97,11 +39,49 @@ Vue.component('top_bar', {
 			<div v-if="selected != 'photos_text'" id="middle_content">
 				<div v-if="selected == 'music_text'" id="middle_inner_content">
 					<div class="info_card">
-						<h1>Kaledoquiz 2018</h1>
-						<p>KaledoQuiz is a competition put on every year by the radio station "the KURE" at Iowa State that involves a lot of trivia and projects in 
-						order to earn points. One of the projects was to create a video with an original song or cover that was done without any conventional instruments. 
-						I spent about 2.5 hours recording and creating this song using things I found on campus and in my room while my friend Austin directed and edited 
-						the video. We had lots of fun making it, and it really pushed my creativity.</p>
+						<h1>Kaleidoquiz</h1>
+						<p>KaleidoQuiz is a competition put on every year by the radio station "the KURE" at Iowa State that involves a lot of trivia and projects in 
+						order to earn points. There is always a musical video challenge that I participate in.</p>
+						<br>
+						<h2>2019</h2>
+						<p>The musical challenge for 2019 involved creating a diss track about ourselves. There was another Nathan in our group that knows how to rap pretty well, so him and I
+						teamed up to make the song and video below. I made a simple trap beat in about 20 minutes while he wrote some disses. I then recorded his voice, mixing and mastering it
+						while him and another teammate of mine recorded a video for it.
+						<br>
+						<br>
+						My music production skills improved an incredible amount compared to the year before. Writing music on a weekly basis also really helped me write something
+						that worked for what we were trying to accomplish.</p>
+						<br>
+						<br>
+						<h2>2018</h2>
+						<p>I worked with my teammate Ausin to create a music video where the prompt was to create a song that didn't use any instruments. I recorded all of the sound in the
+						video from my dorm room and put them together in Ableton Live. Austin and I then went out and recorded a video for it, creating a robot helmet for me to wear.
+						<br>
+						<br>
+						It was a lot of fun and really made me think very creatively. I'm proud of how it came out especially since we only had six hours to do the whole thing.</p>
+							<iframe class="video_box" src="https://www.youtube.com/embed/I_DftkUpqmc" height="480" width="720" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>
+					</div>
+					<div class="info_card">
+						<h1>Tetrad</h1>
+						<p>KaleidoQuiz is a competition put on every year by the radio station "the KURE" at Iowa State that involves a lot of trivia and projects in 
+						order to earn points. There is always a musical video challenge that I participate in.</p>
+						<br>
+						<h2>2019</h2>
+						<p>The musical challenge for 2019 involved creating a diss track about ourselves. There was another Nathan in our group that knows how to rap pretty well, so him and I
+						teamed up to make the song and video below. I made a simple trap beat in about 20 minutes while he wrote some disses. I then recorded his voice, mixing and mastering it
+						while him and another teammate of mine recorded a video for it.
+						<br>
+						<br>
+						My music production skills improved an incredible amount compared to the year before. Writing music on a weekly basis also really helped me write something
+						that worked for what we were trying to accomplish.</p>
+						<br>
+						<br>
+						<h2>2018</h2>
+						<p>I worked with my teammate Ausin to create a music video where the prompt was to create a song that didn't use any instruments. I recorded all of the sound in the
+						video from my dorm room and put them together in Ableton Live. Austin and I then went out and recorded a video for it, creating a robot helmet for me to wear.
+						<br>
+						<br>
+						It was a lot of fun and really made me think very creatively. I'm proud of how it came out especially since we only had six hours to do the whole thing.</p>
 							<iframe class="video_box" src="https://www.youtube.com/embed/I_DftkUpqmc" height="480" width="720" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>
 					</div>
 				</div>
@@ -115,18 +95,18 @@ Vue.component('background_image', {
 	props: ['buttons_visible'],
 	data: function () {
 		return {
-			picture_links: ['src/background_pictures/my_back_mountain.jpg', 
-			'src/background_pictures/Badlands.jpg', 
-			'src/background_pictures/Colorado_Top.jpg', 
-			'src/background_pictures/Glacier1.jpg', 
-			'src/background_pictures/Glacier2.jpg', 
-			'src/background_pictures/Badlands2.jpg', 
-			'src/background_pictures/Swing_Dance.jpg', 
-			'src/background_pictures/Tetons1.jpg', 
-			'src/background_pictures/Tetons2.jpg',
-			'src/background_pictures/Tetons3.jpg',
-			'src/background_pictures/River.jpg',
-			'src/background_pictures/Campsite.jpg'],
+			picture_links: ['src/background_pictures/my_back_mountain.jpg',
+				'src/background_pictures/Badlands.jpg',
+				'src/background_pictures/Colorado_Top.jpg',
+				'src/background_pictures/Glacier1.jpg',
+				'src/background_pictures/Glacier2.jpg',
+				'src/background_pictures/Badlands2.jpg',
+				'src/background_pictures/Swing_Dance.jpg',
+				'src/background_pictures/Tetons1.jpg',
+				'src/background_pictures/Tetons2.jpg',
+				'src/background_pictures/Tetons3.jpg',
+				'src/background_pictures/River.jpg',
+				'src/background_pictures/Campsite.jpg'],
 			current_picture_index: 0,
 			current_timer: setInterval(this.onClickForward, 10000),
 			slide_direction: "slide_left"
@@ -153,7 +133,7 @@ Vue.component('background_image', {
 			this.current_timer = setInterval(this.onClickForward, 10000);
 		},
 	},
-	created: function(){
+	created: function () {
 		this.$store.commit('setTimer')
 	},
 	template: `
@@ -176,7 +156,7 @@ Vue.component('background_image', {
 new Vue({
 	el: '#app',
 	store,
-	data: function(){
+	data: function () {
 		return {
 			photo_buttons_visible: true,
 		}
