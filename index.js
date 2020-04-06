1794525fa15ed68dc2806254c154b93c1a61b81e
nWhitcome <whitcome@iastate.edu>
2020-04-06 12:46:39 -0500

    added https

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+const fs = require('fs');
 var port = process.env.PORT || 3000;
-var server = require('http').createServer(app);
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('origin.pem')
+}
+var server = require('https').createServer(options, app);
 
 app.use(express.static(__dirname));
 app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/index.html');
+    response.sendFile(__dirname + '/src/index.html');
 });
 
 server.listen(port);
